752016bd6ddd644faa7cdadedb5757909076d0c9
Luke Berry <luke@braid.com>
2014-02-27 17:22:27 -0500

    started work on background layer class

--- background.lua
+++ background.lua
@@ -1,8 +1,9 @@
 Background = {}
 Background.__index = Background
 
-function Background.new()
+function Background.new(image, speed)
   local self = setmetatable({}, Background)
-  
+  self.texture = image
+  self.speed = speed
   return self
 end
